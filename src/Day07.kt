319de2e055b2df1b82fd502491b485457a9a87fe
Jiří Pudil <me@jiripudil.cz>
2023-12-09 12:16:09 +0100

    Day07 part 2

--- src/Day07.kt
+++ src/Day07.kt
@@ -12,30 +12,56 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val hands = input.map { line ->
+            val (handInput, bidInput) = line.split(" ")
+            val hand = Hand.parse(handInput, withJokers = true)
+            val bid = bidInput.toInt()
+            Pair(hand, bid)
+        }
+
+        val byRank = hands.sortedBy { it.first }
+        return byRank.mapIndexed { index, pair -> (index + 1) * pair.second }.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day07_test")
     check(part1(testInput) == 6440)
-    // check(part2(testInput) == 0)
+    //check(part2(testInput) == 5905)
 
     val input = readInput("Day07")
     part1(input).println()
-    // part2(input).println()
+    part2(input).println()
 }
 
-class Hand(private val cards: List<Card>) : Comparable<Hand> {
+class Hand(private val cards: List<Card>, private val withJokers: Boolean) : Comparable<Hand> {
     init {
         require(cards.size == 5)
     }
 
-    val strength: Int get() {
-        val counts = cards
-            .groupBy { it }
-            .map { (_, list) -> list.size }
-            .sorted()
-            .reversed()
+    private val strength: Int get() {
+        val groups = cards.groupBy { it }
+            .mapValues { (_, list) -> list.size }
+            .toList()
+            .sortedByDescending { it.second }
+            .toMap()
+            .toMutableMap()
+
+        if (withJokers) {
+            val joker = Card.from('J', withJokers = true)
+            if (groups.contains(joker)) {
+                val jokersCount = groups.getValue(joker)
+                groups.remove(joker)
+
+                if (groups.isEmpty()) {
+                    groups.set(joker, jokersCount)
+                } else {
+                    val bestCard = groups.toList().first()
+                    groups[bestCard.first] = bestCard.second + jokersCount
+                }
+            }
+        }
+
+        val counts = groups.values.toList()
 
         return when {
             counts[0] == 5 -> 7 // five of a kind
@@ -66,9 +92,9 @@ class Hand(private val cards: List<Card>) : Comparable<Hand> {
     }
 
     companion object {
-        fun parse(input: String): Hand {
-            val cards = input.chunked(1).map { Card.from(it) }
-            return Hand(cards)
+        fun parse(input: String, withJokers: Boolean = false): Hand {
+            val cards = input.map { Card.from(it, withJokers) }
+            return Hand(cards, withJokers)
         }
     }
 
@@ -78,14 +104,14 @@ class Hand(private val cards: List<Card>) : Comparable<Hand> {
         }
 
         companion object {
-            fun from(symbol: String): Card {
+            fun from(symbol: Char, withJokers: Boolean): Card {
                 return when (symbol) {
-                    "2", "3", "4", "5", "6", "7", "8", "9" -> Card(symbol.toInt())
-                    "T" -> Card(10)
-                    "J" -> Card(11)
-                    "Q" -> Card(12)
-                    "K" -> Card(13)
-                    "A" -> Card(14)
+                    '2', '3', '4', '5', '6', '7', '8', '9' -> Card(symbol.toString().toInt())
+                    'T' -> Card(10)
+                    'J' -> Card(if (withJokers) 1 else 11)
+                    'Q' -> Card(12)
+                    'K' -> Card(13)
+                    'A' -> Card(14)
                     else -> throw InvalidInput()
                 }
             }
